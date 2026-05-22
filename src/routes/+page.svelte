113524ad029d09ba28da25cb989366788b4f343f
andyyvo <andy@heliux.com>
2026-05-21 23:02:57 -0700

    hero page :DDD

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,33 @@
 <script lang="ts">
+	const SMILEY = ':DDD';
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<section class="hero">
+	<h1 class="smiley">{SMILEY}</h1>
+</section>
+
+<style lang="scss">
+	@import '../lib/styles/global.scss';
+
+	.hero {
+		position: relative;
+		min-height: 100vh;
+		width: 100%;
+		background-color: $white;
+		overflow: hidden;
+	}
+
+	.smiley {
+		position: absolute;
+		top: calc(42vh - 17.16vmin);
+		left: calc(max(24px, 4.5vw) - 1.98vmin);
+		margin: 0;
+		font-family: 'IBM Plex Sans', sans-serif;
+		font-weight: 200;
+		font-size: 22vmin;
+		line-height: 1;
+		letter-spacing: normal;
+		color: $ink;
+		user-select: none;
+	}
+</style>
